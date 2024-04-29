ecca007c73434fbf232b3aa35563e30a8a37e404
aboutwonjun <wjdnjswns@gmail.com>
2024-04-29 17:36:34 +0900

    practice 9

--- C++_Practice/0429_/p8_vector_중복삭제.cpp
+++ C++_Practice/0429_/p8_vector_중복삭제.cpp
@@ -5,7 +5,7 @@
 #include <iostream>
 #include <vector>
 #include<algorithm>
-using namespace std; // Unique 함수를 쓰기 위함
+using namespace std; // Sort, Unique 함수를 쓰기 위함
 
 
 int main(){
@@ -26,9 +26,15 @@ int main(){
 
     cout << "중복 원소 제거 ";
 
+    // 1) Sort 함수로 배열 정렬 
     sort(myVector.begin(), myVector.end());
-    myVector.erase(unique(myVector.begin(),myVector.end()), myVector.end());
+    // 2) Unique 함수 이용해서 중복 값 맨 마지막으로 이동
+    auto newEnd = unique(myVector.begin(), myVector.end());
+    // 3) erase 함수 이용해서 중복 값 범위 삭제
+    myVector.erase(unique(myVector.begin(), myVector.end()), myVector.end());
 
+    //v.erase(first, last); // 범위는 [first,last)
+    //v.unique = 중복된 함수를 맨 뒤로 이동시키는 역할. 
 
     for (int i = 0; i < myVector.size(); i++)
     cout << myVector[i] << " ";
@@ -36,4 +42,29 @@ int main(){
 
     return 0;
     
+}
+
+// 참고 : https://sanghyu.tistory.com/78
+
+// 함수화  - 이나경님 
+void printVector(vector<int> vector)
+{
+	for (int i = 0; i < vector.size(); i++)
+	{
+		cout << vector[i] << " ";
+	}
+	cout << endl << endl;
+}
+
+void uniqueVector(vector<int> vector)
+{
+	sort(vector.begin(), vector.end());
+
+	cout << "myVector sorted: ";
+	printVector(vector);
+
+	vector.erase(unique(vector.begin(), vector.end()), vector.end());
+
+	cout << "myVector unique: ";
+	printVector(vector);
 }
---
+++ C++_Practice/0429_/p9_Dynamic_array.cpp
@@ -0,0 +1,73 @@
+// 2차원 배열 복습 - 행렬 합
+
+#include <iostream>
+using namespace std; 
+
+int main () {
+
+    // 1. 사용자로 부터 행과 열 입력 받기
+    int x;
+    int y;
+
+    //2. 행과 열의 수를 입력받기
+    cout << "행과 열의 수를 입력해주세요. \n"; 
+    cin >> x; 
+    cin >> y;
+
+    //동적 배열 설정 
+    cout << "행렬 원소를 입력해주세요. \n"; 
+    int **arr2 = new int *[x];
+    for (int i = 0; i < x; i++) {
+        arr2[i] = new int[y];
+        for (int j = 0;  j < y; j++){
+            cin >> arr2[i][j];
+        }
+    }
+
+    //3. 행렬 출력하기 
+    for (int i = 0; i < x; i++) { 
+        for (int j = 0; j < y; j++){
+            cout << arr2[i][j] << " "; // 값을 출력하고 공백을 추가
+        }
+        cout << endl; // 한 행이 끝나면 줄바꿈합니다.
+    }
+
+    // 5. 각 행의 합 
+    cout << endl << "각 행의 합 : " << endl;
+	for (int i = 0; i < x; i++)
+	{
+		int rowSum = 0;
+		cout << "행 " << i+1 << ": ";
+
+		for (int j = 0; j < x; j++)
+		{
+			rowSum += arr2[i][j];
+		}
+		cout << rowSum << endl;
+	}
+
+
+    //6. 각 열의 합
+
+	cout << endl << "각 열의 합: " << endl;
+	for (int i = 0; i < y; i++)
+	{
+		int colSum = 0;
+		cout << "열 " << i+1 << ": ";
+
+		for (int j = 0; j < y; j++)
+		{
+			colSum += arr2[j][i];
+		}
+		cout << colSum << endl;
+
+	}
+
+    
+
+    return 0;
+}
+
+
+
+
