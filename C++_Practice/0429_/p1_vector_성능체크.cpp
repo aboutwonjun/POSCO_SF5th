d8dac32857db77d07b4bf97cc712561dbd520ebe
aboutwonjun <wjdnjswns@gmail.com>
2024-04-29 14:46:18 +0900

    0429 Practice

--- C++_Practice/0429_/p1_vector_성능체크.cpp
+++ C++_Practice/0429_/p1_vector_성능체크.cpp
@@ -14,7 +14,7 @@ int main() {
 
     // 1. 크기가 1인 test_vec 생성
 
-    //2. push_back 1억회 수행, ms 단위로 측정 후 출력
+    // 2. push_back 1억회 수행, ms 단위로 측정 후 출력
     clock_t start, end;
     double result;
 
@@ -28,10 +28,12 @@ int main() {
 
     cout << "test_vec 1 수헹시간 : " << result << "millisecond" << "\n";
 
-    // 3. reserve() 1억개 만큼의 공간을 예약.
+    // 3. 크기가 1인 벡터 생성
     vector<int> test_vec2 (1);
-    start = clock();
+    // 4. reserve() 1억개 만큼의 공간을 예약.
     test_vec1.reserve(100000000);
+    start = clock();
+    test_vec2.push_back(100000000);
     end = clock();
 
     result = (double)(end-start);
