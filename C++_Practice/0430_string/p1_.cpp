a344c2a2b88aac6a0466086e003b2866a15e1797
aboutwonjun <wjdnjswns@gmail.com>
2024-04-30 15:13:39 +0900

    0430 string practice1

--- C++_Practice/0430_string/p1_.cpp
+++ C++_Practice/0430_string/p1_.cpp
@@ -1,3 +1,6 @@
+// String 실습 1
+
+
 #include <iostream>
 #include <string>
 using namespace std;
@@ -7,11 +10,11 @@ int main () {
 
     //2. s 문자열 길이 출력
     cout << "입력 문자값의 길이 출력 : ";
-    cout << str.size() << endl;
+    cout << str.length() << endl;
 
     //3. 100번째 문자 출력.
     cout << "100번째 문자출력: ";
-    cout << str.at(100) << endl;
+    cout << str.at(99) << endl;
 
     //4. find 
     cout << "two 문자가 처음 나오는 Index 출력 :";
@@ -20,6 +23,8 @@ int main () {
     //5. two라는 문자가 두번째 나오는 인덱스 출력
     cout << "two 문자가 두번째 나오는 Index 출력: ";
     cout << str.find("two", 12) << endl;
+
+    //cout << "two라는 문자가 처음 나오는 index: " << str.find("two", str.find("two")+1) << endl;
 
 
     return 0;
