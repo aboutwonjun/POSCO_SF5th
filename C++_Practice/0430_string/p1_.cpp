0a8b219418e438867f4ddc44ef91989b5dd4e7d3
aboutwonjun <wjdnjswns@gmail.com>
2024-04-30 15:06:52 +0900

    0430 string practice1

---
+++ C++_Practice/0430_string/p1_.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main () {
+    string str = "Police say two people are suspected of trying to create a shortcut for their construction work.The two have been detained and the case is under further investigation.The 38-year-old man and 55-year-old woman were working near the affected area, the 32nd Great Wall.";
+
+    //2. s 문자열 길이 출력
+    cout << "입력 문자값의 길이 출력 : ";
+    cout << str.size() << endl;
+
+    //3. 100번째 문자 출력.
+    cout << "100번째 문자출력: ";
+    cout << str.at(100) << endl;
+
+    //4. find 
+    cout << "two 문자가 처음 나오는 Index 출력 :";
+    cout << str.find("two") << endl;
+
+    //5. two라는 문자가 두번째 나오는 인덱스 출력
+    cout << "two 문자가 두번째 나오는 Index 출력: ";
+    cout << str.find("two", 12) << endl;
+
+
+    return 0;
+
+
+
+
+
+
+}
