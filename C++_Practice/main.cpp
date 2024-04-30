b90e90501067442202400c410a0179be0b013618
aboutwonjun <wjdnjswns@gmail.com>
2024-04-30 14:23:19 +0900

    0429 Practice

---
+++ C++_Practice/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  POOSCO
+//
+//  Created by Wonjun Lee on 4/30/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
