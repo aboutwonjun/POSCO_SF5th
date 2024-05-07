fa5c16d71fdee1ed703e2e650da62bb1ec184cdf
aboutwonjun <wjdnjswns@gmail.com>
2024-05-07 16:26:12 +0900

    0507 practice1

--- C++_Practice/0507/FileInputOuput.cpp
+++ C++_Practice/0507/FileInputOuput.cpp
@@ -12,8 +12,8 @@ int main()
 {   
     // 1) 기존 Hello txt 파일 읽어오기 
     ifstream file("hello.txt");
-    string line;
 
+    //hint 벡터 사용하기! 
     vector <string> v;
     string s;
 
@@ -28,9 +28,16 @@ int main()
     //3) Reverse 함수로 순서 변경하기 
     reverse(v.begin(), v.end());
 
+    cout << "Reverse 결과 확인" << endl;
+
+    for (int i = 0; i < v.size(); i++)
+    {
+        cout << v[i]<< endl;
+    }
+
     //4) 새로운 파일 열어서 입력하고 닫기
     ofstream write_file("new_hello.txt");
-    for (const auto& str : v)
+    for (const auto& str : v) 
     {
         write_file << str << endl;
     }
